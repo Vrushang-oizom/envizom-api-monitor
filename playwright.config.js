1dfbd7d2bd93c2cec59afac304ccfef73cf7d714
Vrushang-oizom <webqc@oizom.com>
2026-02-12 12:11:08 +0530

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -2,8 +2,8 @@ const { defineConfig } = require('@playwright/test');
 
 module.exports = defineConfig({
   timeout: 120000,
+  workers: 1,
   use: {
-    headless: true   // REQUIRED for GitHub Actions
+    headless: true
   }
 });
-
